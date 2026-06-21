d01f95448539f5e6374cff57b1d4a49296991c67
lightning4747 <Vignesh112847@gmail.com>
2026-06-21 18:32:44 +0530

    1987-SUBSTRINGS-OF-SIZE-THREE-WITH-DISTINCT-CHARACTERS Stats: Time: 1 ms (96.76%), Space: 42.9 MB (57.81%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -2,18 +2,15 @@ class Solution {
     public int countGoodSubstrings(String s) {
         if(s.length() < 3) return 0;
 
-        int i =0,j = 2;
+        int j = 0;
         int count = 0;
 
-        while(j<s.length()) {
-            char[] arr = s.substring(i,j+1).toCharArray();
-            Set<Character> set = new HashSet<>();
-            for(char ch : arr) {
-                set.add(ch);
-            }
-            System.out.print(set);
-            if(set.size() == 3) count++;
-            i++;j++;
+        while(j<s.length()-2) {
+            char a = s.charAt(j);
+            char b = s.charAt(j+1);
+            char c = s.charAt(j+2);
+            if(a != b && b != c && a != c) count++;
+            j++;
         }
         return count;
     }
