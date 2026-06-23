8003c162a39ef9d5a565f49557ce19c79e58bffe
lightning4747 <Vignesh112847@gmail.com>
2026-06-23 09:56:28 +0530

    2761-ARRAY-REDUCE-TRANSFORMATION Stats: Time: 52 ms (23.97%), Space: 56.1 MB (50%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.ts
@@ -0,0 +1,9 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    let res: number = init;
+    for(let i =0;i<nums.length;i++) {
+        res = fn(res, nums[i]);
+    }
+    return res;
+};
